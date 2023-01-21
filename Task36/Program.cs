67d76a75b404834cbbf5dc062c163245163c842b
Tatiana Denisova <2746619@gmail.com>
2023-01-21 22:51:38 +0300

    Add Task36

---
+++ Task36/Program.cs
@@ -0,0 +1,49 @@
+﻿/* Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0*/
+int[] FullArray(int length, int start, int end)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(start, end + 1);
+    }
+    return array;
+}
+int getUserData(string message)
+{
+    Console.WriteLine(message);
+    int userData = int.Parse(Console.ReadLine()!);
+    return userData;
+}
+int SumOddPosition(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum = sum + array[i];
+        }
+    }
+    return sum;
+}
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.WriteLine("]");
+}
+int number = getUserData("Введите количество элемнтов массива");
+int[] array = FullArray(number, -9, 9);
+PrintArray(array);
+int sum = SumOddPosition(array);
+Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях равна {sum}");
